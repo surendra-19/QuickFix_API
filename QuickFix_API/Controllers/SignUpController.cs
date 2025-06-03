dfb4ef108624eb4459f615245041b1ecc7b1341b
surendra-19 <surendrachagantipati@gmail.com>
2025-06-03 23:36:06 +0530

    controllers added

---
+++ QuickFix_API/Controllers/SignUpController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickFix_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpController : ControllerBase
+    {
+    }
+}
