ba2c3b224575a29d8c9f60c67e2eb663a05afbdd
surendra-19 <surendrachagantipati@gmail.com>
2025-06-09 23:15:23 +0530

    modified controllers

---
+++ QuickFix_API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuickFix_API.Data;
+using QuickFix_API.Models;
+
+namespace QuickFix_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly QuickFixDBContext _context;
+        public AuthController(QuickFixDBContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("Signup")]
+        public async Task<IActionResult> CustomerRegistration(Customer customer)
+        {
+            var newCustomer = new Customer
+            {
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Email = customer.Email,
+                Password = customer.Password,
+                CityLivingIn = customer.CityLivingIn
+            };
+            _context.Customers.Add(newCustomer);
+            await _context.SaveChangesAsync();
+            return Ok(newCustomer);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> CustomerLogin(Login login)
+        {
+            var email = login.Email;
+            var password = login.Password;
+            var existingCustomer = await _context.Customers.Where(
+                    customer => customer.Email == email && customer.Password == password
+                ).ToListAsync();
+            if (existingCustomer.Count == 0)
+            {
+                return NotFound("Invalid email or password");
+            }
+            return Ok(existingCustomer);
+        }
+    }
+}
---
+++ QuickFix_API/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace QuickFix_API.Models
+{
+    public class Login
+    {
+        public string Email { get; set; } ="";
+        public string Password { get; set; } = "";
+    }
+}
