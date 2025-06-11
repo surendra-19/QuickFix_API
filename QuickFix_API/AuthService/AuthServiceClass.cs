a89b7c5dfefa18c8b040e52f57663c9972771849
Rushikesh K <v-rushikeshk@microsoft.com>
2025-06-11 23:47:09 +0530

    Updated JWT and created  service class for Jwt

---
+++ QuickFix_API/AuthService/AuthServiceClass.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using QuickFix_API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace QuickFix_API.AuthService
+{
+    public class AuthServiceClass
+    {
+        private readonly IConfiguration _config;
+        public AuthServiceClass(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string GenerateJwtToken(Customer customer)
+        {
+            var jwtKey = _config["Jwt:Key"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, customer.Customer_Id.ToString()),
+                new Claim(ClaimTypes.Name, customer.Email),
+                new Claim(ClaimTypes.Role, "Customer")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- QuickFix_API/Program.cs
+++ QuickFix_API/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using QuickFix_API.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,30 @@ builder.Services.AddDbContext<QuickFixDBContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var jwts = builder.Configuration.GetSection("JWT");
+var key = Encoding.UTF8.GetBytes(jwts["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwts["Issuer"],
+        ValidAudience = jwts["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
